ec1fd8f475e11276a02a2f30f1db1a3180eabdc1
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-05-19 22:33:40 -0500

    adicion de listas y servicio

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeseosService {
 
-  listas: any[] = [];
+  listas: Lista[] = [];
 
-  constructor() { }
+  constructor() {
+    console.log('servicio de litas iniciado');
+  }
 }
